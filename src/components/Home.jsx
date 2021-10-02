e15f18b8285fe00f819193b539b8b6f85b96adb3
bernal1227 <bernal1227@gmail.com>
2021-10-02 14:28:23 -0500

    Update home with /p

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="cont-home">
                 <h1>Bienvenido al sistema de Gestión de la Papelería San Vicente!</h1>
                 <h2>Ubicados en el barrio 7 de Agosto en Bogotá, Colombia. Con más de 50 años de expericiencia en la comercialización de productos escolares, de oficina y obsequios</h2>
-                <h3>Aquí puede realizar tareas de gestión de ventas como registrar, actualizar y consultar ventas. Consultar nuestro portafolio de productos</h3>
+                <p>Aquí puede realizar tareas de gestión de ventas como registrar, actualizar y consultar ventas. Consultar nuestro portafolio de productos</p>
             </div>
             <Contact/>
         </div>
