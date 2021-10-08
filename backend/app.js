20b852e2e726c0f5dc293dc8d6281217a6b39402
bernal1227 <bernal1227@gmail.com>
2021-10-08 18:33:35 -0500

    Update Connection DataBase and model product

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
 
+const userDB = "dbUserApp";
+const passDB = "QsSK8JN1shm2neR6";
+const dBase = "GestionSanVicente";
+
 const productsRoutes = require("./routes/products");
 
 app.use(express.json());
@@ -9,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect(
-    "mongodb+srv://dbUser:AeSlz7E0YxzVZgZR@cluster0.wpilz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://"+userDB+":"+passDB+"@sanvicente.l1mmx.mongodb.net/"+dBase+"?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Estamos conectados");
