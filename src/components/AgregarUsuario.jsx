62845f4fcc3425052e43768cce66e8eb933df4ef
Juan <camiloflstudio@gmail.com>
2021-10-15 01:00:25 -0500

    Front users

--- src/components/AgregarUsuario.jsx
+++ src/components/AgregarUsuario.jsx
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button'
 import {Component} from 'react'
 import Table from "react-bootstrap/Table"
 import swal from 'sweetalert'
+import "../styles/AgregarUsuario.css"
+
 
 class AgregarUsuario extends Component{
     constructor(props) {
@@ -103,6 +105,15 @@ class AgregarUsuario extends Component{
 
 render(){
     return(
+      <>
+      <div className="father-cont"> 
+          <h2> Sistema de gestión usuarios</h2>{' '}
+      </div>
+        <div className="arrow">    
+              <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+              </svg>
+        </div>    
         <Container>
         <Form onSubmit={this.addUser}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -119,13 +130,34 @@ render(){
                 <Form.Label>Rol</Form.Label>
                 <Form.Control name="estado" onChange={this.handleChange} value={this.state.estado} type="text" placeholder="Ingrese Rol" />
             </Form.Group>
+            <div className="boton">
+            <h9>Agrega un usuario y dale potestad de administrador o<br/>usuario. Elimina y actualiza información.</h9>
             
+            <div className="cont-icons">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                  <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                  <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"/>
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                  <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                  </svg>
+            </div>
+
             <Button variant="primary" type="submit" className="justify-content center" >
                 Agregar Usuario
             </Button>
+            </div>
 </Form>
 <br />
+<div className="break">
 
+</div>
 <Table striped bordered hover>
           <thead>
             <tr>
@@ -160,6 +192,7 @@ render(){
           </tbody>
         </Table>
 </Container>
+</>
     )
 }
 }
